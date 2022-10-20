c890749e0e03f86c3ad318d4bb6ad451f7d13684
basilmt <57865169+basilmt@users.noreply.github.com>
2022-10-20 13:16:11 +0000

    stack and queues

---
+++ Stack/stack_using_ll.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+
+    def printList(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+        return True
+
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp
+
+
+s = Stack(4)
+s.push(3)
+s.push(2)
+print(s.pop().value, "\n")
+s.printList()
